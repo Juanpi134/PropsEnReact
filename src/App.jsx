0fced698ac07faba86f00d1ba51fd2abdff49542
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-05 08:38:53 -0300

    se añaden nuevos estudiantes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Estudiantes from './Estudiantes'
 function App(){
   return <div>
     <Estudiantes name="Juan" age="20" isStudent={true}/>
+    <Estudiantes name="Alberto" age={50} isStudent={false}/>
+    <Estudiantes name="Raul" age={30} isStudent={true}/>
   </div>
 }
 
